69cac663807f96b0ae85c4bb482aa62996c6ca89
Robert Sanders <45695926+robSanders818@users.noreply.github.com>
2020-09-14 23:24:12 -0400

    Fix non update

--- indexPage.js
+++ indexPage.js
@@ -5,7 +5,7 @@ export default class IndexPage extends React.Component {
     render() {
         return <div>
             <h1 style={{fontSize: 18}}>
-                Hi there, I'm Robert Sanders!
+                Hi there, I'm Robert Sanders
             </h1>
             <p style={{fontSize: 14}}>
                 <div dangerouslySetInnerHTML={{__html: bioMainText}} />;
