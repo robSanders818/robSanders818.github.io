171f262dd8c0f5ae1f88f3eb1c94c2c116c82b68
Robert Sanders <45695926+robSanders818@users.noreply.github.com>
2020-09-14 22:14:55 -0400

    Converted index to React

---
+++ index_page.js
@@ -0,0 +1,19 @@
+import {bioMainText, bioSubText} from "./constants"
+class IndexPage extends React.Component {
+    render() {
+        return <div>
+            <h1 style={{fontSize: 18}}>
+                Hi there, I'm Robert Sanders!
+            </h1>
+            <p style={{fontSize: 14}}>
+                <div dangerouslySetInnerHTML={{__html: bioMainText}} />;
+            </p>
+            <h2 style={{fontSize: 16}}>
+                <p>
+                    <div dangerouslySetInnerHTML={{__html: bioMainText}} />;
+                </p>
+            </h2>
+        </div>
+          
+    }
+}
